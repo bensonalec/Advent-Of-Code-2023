980f4b075a2782609dfcaac67006883943fe2360
Alec Benson <alecbenson21@gmail.com>
2023-12-05 18:18:47 -0700

    Cleaned up Day 5 code a bit

--- lib/day5.ex
+++ lib/day5.ex
@@ -8,35 +8,11 @@ defmodule Aoc.Day5 do
     do_part_1(inp)
   end
 
-  defp between(st, en, val) do
-    val >= st  and val <= en
-  end
-
   defp do_part_1(inp) do
     first_line = String.split(inp, "\n") |> Enum.at(0)
     seeds = Regex.scan(~r/\d+/, first_line) |> List.flatten |> Enum.map(fn z -> elem(Integer.parse(z),0) end)
-    mappings = Regex.scan(~r/(?<=map:\n)(?:\d+ \d+ \d+\n)+\d+ \d+ \d+/, inp)
-    |> List.flatten
-    |> Enum.map(fn x ->
-      String.split(x, "\n")
-      |> Enum.map(fn y ->
-        [dest, source, range] = Regex.scan(~r/\d+/, y) |> List.flatten |> Enum.map(fn z -> elem(Integer.parse(z),0) end)
-        range = range - 1
-        {{source,source+range}, dest}
-      end)
-    end)
-    Enum.map(seeds, fn x ->
-      Enum.reduce(mappings, x, fn y, acc ->
-        Enum.reduce_while(y, acc, fn z, acc_z ->
-          {{st, en}, dest_start} = z
-          if between(st, en, acc_z) do
-            {:halt, abs(st-acc_z) + dest_start}
-          else
-            {:cont, acc_z}
-          end
-        end)
-      end)
-    end) |> Enum.min
+    mappings = create_mappings(inp, :dest_source)
+    Enum.map(seeds, fn x -> map_value(mappings, x) end) |> Enum.min
   end
 
   def part2 do
@@ -52,37 +28,57 @@ defmodule Aoc.Day5 do
     first_line = String.split(inp, "\n") |> Enum.at(0)
     seeds = Regex.scan(~r/\d+/, first_line) |> List.flatten |> Enum.map(fn z -> elem(Integer.parse(z),0) end)
     |> Enum.chunk_every(2)
-    mappings = Regex.scan(~r/(?<=map:\n)(?:\d+ \d+ \d+\n)+\d+ \d+ \d+/, inp)
-    |> List.flatten
-    |> Enum.map(fn x ->
-      String.split(x, "\n")
-      |> Enum.map(fn y ->
-        [source, dest, range] = Regex.scan(~r/\d+/, y) |> List.flatten |> Enum.map(fn z -> elem(Integer.parse(z),0) end)
-        range = range - 1
-        {{source,source+range}, dest}
-      end)
-    end)
+    mappings = create_mappings(inp, :source_dest)
     mappings = mappings |> Enum.reverse
     inp = (0..100000000000000000000000000)
     Enum.reduce_while(inp, {}, fn x, _ ->
-      val = Enum.reduce(mappings, x, fn y, acc ->
-        Enum.reduce_while(y, acc, fn z, acc_z ->
-          {{st, en}, dest_start} = z
-          if between(st, en, acc_z) do
-            {:halt, abs(st-acc_z) + dest_start}
-          else
-            {:cont, acc_z}
-          end
-        end)
-      end)
-      Enum.reduce(seeds, false, fn i, acc ->
-        [start, en] = i
-        acc || between(start, start+en-1, val)
-      end)
+      val = map_value(mappings, x)
+      is_value_in_ranges(seeds, val)
       |> case do
         true -> {:halt, x}
         false -> {:cont, x}
       end
     end)
   end
+
+  defp between(st, en, val) do
+    val >= st  and val <= en
+  end
+
+  defp map_value(mappings, value) do
+    Enum.reduce(mappings, value, fn y, acc ->
+      Enum.reduce_while(y, acc, fn z, acc_z ->
+        {{st, en}, dest_start} = z
+        if between(st, en, acc_z) do
+          {:halt, abs(st-acc_z) + dest_start}
+        else
+          {:cont, acc_z}
+        end
+      end)
+    end)
+  end
+
+  defp is_value_in_ranges(ranges, val) do
+    Enum.reduce(ranges, false, fn i, acc ->
+      [start, en] = i
+      acc || between(start, start+en-1, val)
+    end)
+  end
+
+  defp create_mappings(inp, source_order) do
+    Regex.scan(~r/(?<=map:\n)(?:\d+ \d+ \d+\n)+\d+ \d+ \d+/, inp)
+    |> List.flatten
+    |> Enum.map(fn x ->
+      String.split(x, "\n")
+      |> Enum.map(fn y ->
+        [dest, source, range] = Regex.scan(~r/\d+/, y) |> List.flatten |> Enum.map(fn z -> elem(Integer.parse(z),0) end)
+        range = range - 1
+        case source_order do
+          :dest_source -> {{source,source+range}, dest}
+          :source_dest -> {{dest,dest+range}, source}
+        end
+      end)
+    end)
+  end
+
 end
